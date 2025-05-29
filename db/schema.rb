7d026e13dea5ca61f7b5d788f9f841b57d9d6adb
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-05-29 22:57:59 +0700

    Add rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_155432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "quests", force: :cascade do |t|
+    t.string "name"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
