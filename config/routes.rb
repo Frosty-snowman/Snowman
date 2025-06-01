b8b07027540a1dafdba16e9d9e73e280a3486fcc
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-06-01 22:13:21 +0700

    add brag

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get "pages/brag"
   resources :frosty_quests
   root "frosty_quests#index"
 
 
   get "up" => "rails/health#show", as: :rails_health_check
 # config/routes.rb
-  get "/brags", to: "brags#index", as: "brags"
+  # get "/brags", to: "brags#index", as: "brags"
+  get "brags", to: "pages#brag", as: "brags"
+
+
 
 
 end
